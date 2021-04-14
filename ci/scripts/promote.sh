fb7e267790e647960830a3d7007943366e46a39b
Dylan Roberts <dylanr@vmware.com>
2021-04-14 10:51:15 -0400

    Update release pipeline  - migrate from bintray to sonatype-direct release  - use concourse-release-scripts executable jar build by boot team

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -2,38 +2,12 @@
 set -euo pipefail
 
 # shellcheck source=scripts/common.sh
-source "$(dirname "$0")/common.sh"
+source $(dirname $0)/common.sh
 
-buildName=$(cat artifactory-repo/build-info.json | jq -r '.buildInfo.name')
-buildNumber=$(cat artifactory-repo/build-info.json | jq -r '.buildInfo.number')
-version=$(cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/')
+version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
+export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
-if [[ $RELEASE_TYPE == "M" ]]; then
-  targetRepo="libs-milestone-local"
-elif [[ $RELEASE_TYPE == "RC" ]]; then
-  targetRepo="libs-milestone-local"
-elif [[ $RELEASE_TYPE == "RELEASE" ]]; then
-  targetRepo="libs-release-local"
-else
-  echo "Unknown release type $RELEASE_TYPE" >&2
-  exit 1
-fi
-
-echo "Promoting ${buildName}/${buildNumber} to ${targetRepo}"
-
-curl \
-  -s \
-  --connect-timeout 240 \
-  --max-time 900 \
-  -u "${ARTIFACTORY_USERNAME}":"${ARTIFACTORY_PASSWORD}" \
-  -H "Content-type:application/json" \
-  -d "{\"status\": \"staged\", \"sourceRepo\": \"libs-staging-local\", \"targetRepo\": \"${targetRepo}\"}" \
-  -f \
-  -X \
-  POST "${ARTIFACTORY_SERVER}/api/build/promote/${buildName}/${buildNumber}" >/dev/null || {
-  echo "Failed to promote" >&2
-  exit 1
-}
+java -jar /spring-boot-release-scripts.jar promote $RELEASE_TYPE $BUILD_INFO_LOCATION
 
 echo "Promotion complete"
-echo $version >version/version
+echo $version > version/version
