48113fabc406a91adcf00516c1be7f2dd3112858
Gareth Clay <gclay@pivotal.io>
2021-07-02 12:19:55 +0100

    206 Create scs-starters' own ci image
    * Stop using the App Broker base ci image
    * Bump concourse-release-scripts
    * Tidy pipeline in line with other SCS OSS projects

---
+++ scripts/set-pipelines.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly FLY_TARGET=scs
+
+main() {
+  fly -t "$FLY_TARGET" sync
+
+  pushd "$(dirname $0)/../ci" > /dev/null
+    echo "Setting starters pipeline..."
+    fly --target "$FLY_TARGET" set-pipeline --pipeline starters \
+      --config pipeline.yml \
+      --load-vars-from config-concourse.yml
+  popd > /dev/null
+}
+
+main
