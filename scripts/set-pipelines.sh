84b7a05f27ddcfbda5217b18f10dc5970150169b
Gareth Clay <gclay@pivotal.io>
2021-07-02 12:32:33 +0100

    Add build-ci-images pipeline job
    Also:
    * Rename pipeline to starters-3.3.x

--- scripts/set-pipelines.sh
+++ scripts/set-pipelines.sh
@@ -9,7 +9,7 @@ main() {
 
   pushd "$(dirname $0)/../ci" > /dev/null
     echo "Setting starters pipeline..."
-    fly --target "$FLY_TARGET" set-pipeline --pipeline starters \
+    fly --target "$FLY_TARGET" set-pipeline --pipeline starters-3.3.x \
       --config pipeline.yml \
       --load-vars-from config-concourse.yml
   popd > /dev/null
