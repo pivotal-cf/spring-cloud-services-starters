69766d599745360e032e679ce730df3741cfa8ec
kvmw <mshamsi@vmware.com>
2022-07-22 12:21:46 -0400

    Prepare 3.4.x branch for Spring Boot 2.6.x

--- scripts/set-pipelines.sh
+++ scripts/set-pipelines.sh
@@ -12,7 +12,7 @@ main() {
     fly --target "$FLY_TARGET" set-pipeline --pipeline starters-3.4.x \
       --config pipeline.yml \
       --load-vars-from config-concourse.yml \
-      --var branch="main"
+      --var branch="3.4.x"
   popd > /dev/null
 }
 
