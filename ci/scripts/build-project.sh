4f0fa489e3dff7a9bef646d97bee884c0591f5bc
Kaveh Shamsi <mshamsi@vmware.com>
2022-11-23 16:54:24 +0100

    Make SCS Starters ready for spring-boot 3 (#311)
    * Updates gradle version to 7.5.1
    
    * Migrates to new maven-publish plugin
    
    * Next development version (v3.6.0-SNAPSHOT)
    
    * Updates spring-boot and spring-cloud dependencies
    
    * Fixes the java version in github actions

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -6,5 +6,5 @@ source "$(dirname "$0")/common.sh"
 repository=$(pwd)/distribution-repository
 
 pushd git-repo >/dev/null
-./gradlew --no-daemon clean build install -Dmaven.repo.local="${repository}" -PbuildmasterName="${ARTIFACTORY_USERNAME}" -PbuildmasterPass="${ARTIFACTORY_PASSWORD}"
+./gradlew --no-daemon clean build publishToMavenLocal -Dmaven.repo.local="${repository}" -PbuildmasterName="${ARTIFACTORY_USERNAME}" -PbuildmasterPass="${ARTIFACTORY_PASSWORD}"
 popd >/dev/null
