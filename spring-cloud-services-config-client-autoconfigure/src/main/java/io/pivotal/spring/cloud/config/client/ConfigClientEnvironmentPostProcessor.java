f49fa64fc2c7ad9d282eb04c425263a281871ed8
kvmw <mshamsi@vmware.com>
2023-01-04 10:41:15 +0100

    Adds oauth2 properties

--- spring-cloud-services-config-client-autoconfigure/src/main/java/io/pivotal/spring/cloud/config/client/ConfigClientEnvironmentPostProcessor.java
+++ spring-cloud-services-config-client-autoconfigure/src/main/java/io/pivotal/spring/cloud/config/client/ConfigClientEnvironmentPostProcessor.java
@@ -15,21 +15,19 @@
  */
 package io.pivotal.spring.cloud.config.client;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.pivotal.cfenv.core.CfCredentials;
+import io.pivotal.cfenv.core.CfEnv;
+import io.pivotal.cfenv.core.CfService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.config.ConfigDataEnvironmentPostProcessor;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
-
-import io.pivotal.cfenv.core.CfCredentials;
-import io.pivotal.cfenv.core.CfEnv;
-import io.pivotal.cfenv.core.CfService;
 
 /**
  * Using {@link CfEnv} directly here as we need to set the
@@ -51,15 +49,33 @@ public class ConfigClientEnvironmentPostProcessor implements EnvironmentPostProc
 		if (configServices.size() != 1)
 			return;
 		CfCredentials credentials = configServices.stream().findFirst().get().getCredentials();
-		Map<String, Object> map = new HashMap<>();
-		map.put("spring.config.import", "optional:configserver:" + credentials.getUri());
-		map.put("spring.cloud.refresh.additional-property-sources-to-retain", SPRING_CLOUD_SERVICES_CONFIG_IMPORT);
-		environment.getPropertySources().addFirst(new MapPropertySource(SPRING_CLOUD_SERVICES_CONFIG_IMPORT, map));
+		environment.getPropertySources().addFirst(oauth2PropertySource(credentials));
+		environment.getPropertySources().addFirst(configImportPropertySource(credentials));
 	}
 
 	@Override
 	public int getOrder() {
 		return ConfigDataEnvironmentPostProcessor.ORDER - 1;
+	}
+
+	private MapPropertySource configImportPropertySource(CfCredentials credentials) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("spring.config.import", "optional:configserver:" + credentials.getUri());
+		map.put("spring.cloud.refresh.additional-property-sources-to-retain", SPRING_CLOUD_SERVICES_CONFIG_IMPORT);
+		return new MapPropertySource(SPRING_CLOUD_SERVICES_CONFIG_IMPORT, map);
+	}
+
+	/**
+	 * This method can be removed once java-cfenv supports config-client.
+	 */
+	private MapPropertySource oauth2PropertySource(CfCredentials credentials) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("spring.cloud.config.uri", credentials.getUri());
+		map.put("spring.cloud.config.client.oauth2.client-id", credentials.getString("client_id"));
+		map.put("spring.cloud.config.client.oauth2.client-secret", credentials.getString("client_secret"));
+		map.put("spring.cloud.config.client.oauth2.access-token-uri", credentials.getString("access_token_uri"));
+
+		return new MapPropertySource("CfConfigClientProcessor", map);
 	}
 
 }
