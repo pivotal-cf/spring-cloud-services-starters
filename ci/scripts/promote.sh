c7b7319daad7203d031b6ee3d697db080d393760
kvmw <mshamsi@vmware.com>
2024-05-30 11:37:27 +0200

    wip
    Signed-off-by: kvmw <mshamsi@vmware.com>

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 # shellcheck source=scripts/common.sh
 source $(dirname $0)/common.sh
 
-version=$(cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/')
-export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
+version=$(cat artifactory/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/')
+export BUILD_INFO_LOCATION=$(pwd)/artifactory/build-info.json
 
 java -jar /concourse-release-scripts*.jar promote $RELEASE_TYPE $BUILD_INFO_LOCATION
 
--- ci/scripts/sync-to-maven-central.sh
+++ ci/scripts/sync-to-maven-central.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-readonly BUILD_INFO_LOCATION="$(pwd)/artifactory-repo/build-info.json"
+readonly BUILD_INFO_LOCATION="$(pwd)/artifactory/build-info.json"
 readonly CONFIG_DIR="$(pwd)/git-repo/ci/config"
 
 java -jar /opt/concourse-release-scripts*.jar \
-  --spring.config.location="${CONFIG_DIR}/release-scripts.yml" \
-  publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" "artifactory-repo"
+	--spring.config.location="${CONFIG_DIR}/release-scripts.yml" \
+	publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" "artifactory"
 
 echo "Sync complete"
