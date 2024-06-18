e7736dfcdb489a96d4a4b5927993fdb9007eb11f
Kaveh Shamsi <mshamsi@vmware.com>
2024-06-18 08:45:05 +0200

    Fixes the jar path in sync-to-maven task (#424)
    Signed-off-by: kvmw <mshamsi@broadcom.com>
    Co-authored-by: kvmw <mshamsi@broadcom.com>

--- ci/scripts/sync-to-maven-central.sh
+++ ci/scripts/sync-to-maven-central.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 readonly BUILD_INFO_LOCATION="$(pwd)/artifactory/build-info.json"
 readonly CONFIG_DIR="$(pwd)/src/ci/config"
 
-java -jar /opt/concourse-release-scripts*.jar \
+java -jar /concourse-release-scripts*.jar \
 	--spring.config.location="${CONFIG_DIR}/release-scripts.yml" \
 	publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" "artifactory"
 
